327feea55bcf2d4a0a05bd28eaf040cacbd596c1
Arun Reghunath <arunreghunath@hotmail.com>
2019-12-20 01:27:32 +0000

    Files Deleted

--- Task3/base_IoT_Assignment_code/client/Python/client.py
+++
@@ -1,8 +0,0 @@
-#Base Python client for MEng in IoT Assignment
-#consumes data from IoT Gateway
-import urllib2
-
-response = urllib2.urlopen('http://localhost:8080/')
-resp = response.read()
-
-print resp
